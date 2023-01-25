fef99acb84f1ef68b22855e18b3f1aeb351bba2d
Ken <kennethfletcher977@yahoo.com>
2023-01-25 05:51:50 -0700

    added simple javascript

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// var topic = "HTML";
+// if (topic === 'HTML') {
+//     console.log("Let's study HTML");
+// } else {
+//     console.log("this is getting dumb");
+// }
+
+var shapes = ["triangle","square","pentagon","circle"];
+for( var x = 0; x < shapes.length; x++ ) {
+    console.log(shapes[x]);
+}
+
